af8ccc31654b55c430e780d5ad7eb286fb395be2
Chaouki Bencherif <chaouki.bencherif1@ibm.com>
2022-10-12 13:50:33 +0100

    first commit

---
+++ Guidomia-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia-iOS
+//
+//  Created by chaouki bencherif on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
